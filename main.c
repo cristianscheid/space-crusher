df5b5a2bf4b870ee93e27488a37bd6beb15bbf49
cristian <cristian@localhost.localdomain>
2024-03-09 17:26:18 -0300

    fix: adjust order of functions and set objects to static for testing

--- main.c
+++ main.c
@@ -7,8 +7,6 @@
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 720
-#define FPS 30
-#define FRAME_TARGET_TIME (1000 / FPS)
 
 ///////////////////////////////////////////////////////////////////////////////
 // Global variables
@@ -57,8 +55,6 @@ struct game_object {
 struct explosion {
     SDL_Rect rect;
     SDL_Texture *tex;
-    float x;
-    float y;
     float count;
     bool is_visible;
 } explosions[8];
@@ -99,7 +95,128 @@ int initialize_window(void) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// Function to poll SDL events and process keyboard input
+// Initial setup
+///////////////////////////////////////////////////////////////////////////////
+void initial_setup(void) {
+
+    // srand initialization - generate random numbers with rand()
+    srand(time(NULL));
+
+    // Create surfaces
+    SDL_Surface *background_sfc = IMG_Load("assets/background.png");
+    SDL_Surface *player_sfc = IMG_Load("assets/player.png");
+    SDL_Surface *laser_sfc = IMG_Load("assets/laser.png");
+    SDL_Surface *enemies_sfcs[14];
+    enemies_sfcs[0] = IMG_Load("assets/enemy-blue-01.png");
+    enemies_sfcs[1] = IMG_Load("assets/enemy-blue-02.png");
+    enemies_sfcs[2] = IMG_Load("assets/enemy-brown-01.png");
+    enemies_sfcs[3] = IMG_Load("assets/enemy-brown-02.png");
+    enemies_sfcs[4] = IMG_Load("assets/enemy-green-01.png");
+    enemies_sfcs[5] = IMG_Load("assets/enemy-green-02.png");
+    enemies_sfcs[6] = IMG_Load("assets/enemy-orange-01.png");
+    enemies_sfcs[7] = IMG_Load("assets/enemy-orange-02.png");
+    enemies_sfcs[8] = IMG_Load("assets/enemy-purple-01.png");
+    enemies_sfcs[9] = IMG_Load("assets/enemy-purple-02.png");
+    enemies_sfcs[10] = IMG_Load("assets/enemy-red-01.png");
+    enemies_sfcs[11] = IMG_Load("assets/enemy-red-02.png");
+    enemies_sfcs[12] = IMG_Load("assets/enemy-yellow-01.png");
+    enemies_sfcs[13] = IMG_Load("assets/enemy-yellow-02.png");
+    SDL_Surface *asteroids_sfcs[2];
+    asteroids_sfcs[0] = IMG_Load("assets/asteroid-01.png");
+    asteroids_sfcs[1] = IMG_Load("assets/asteroid-02.png");
+    SDL_Surface *explosion_sfc = IMG_Load("assets/explosion.png");
+
+    // Create textures
+    background_tex = SDL_CreateTextureFromSurface(renderer, background_sfc);
+    SDL_FreeSurface(background_sfc);
+    player_tex = SDL_CreateTextureFromSurface(renderer, player_sfc);
+    SDL_FreeSurface(player_sfc);
+    laser_tex = SDL_CreateTextureFromSurface(renderer, laser_sfc);
+    SDL_FreeSurface(laser_sfc);
+    for (int i = 0; i < sizeof(enemies_texs) / sizeof(enemies_texs[0]); i++) {
+        enemies_texs[i] = SDL_CreateTextureFromSurface(renderer, enemies_sfcs[i]);
+        SDL_FreeSurface(enemies_sfcs[i]);
+    }
+    for (int i = 0; i < sizeof(asteroids_texs) / sizeof(asteroids_texs[0]); i++) {
+        asteroids_texs[i] = SDL_CreateTextureFromSurface(renderer, asteroids_sfcs[i]);
+        SDL_FreeSurface(asteroids_sfcs[i]);
+    }
+    explosion_tex = SDL_CreateTextureFromSurface(renderer, explosion_sfc);
+
+    // Get background sprite dimensions
+    int background_img_width, background_img_height;
+    SDL_QueryTexture(background_tex, NULL, NULL, &background_img_width, &background_img_height);
+    // Initialize background object
+    backgrounds[0].x = backgrounds[1].x = 0;
+    backgrounds[0].y = WINDOW_HEIGHT - background_img_height;
+    backgrounds[1].y = -background_img_height;
+    backgrounds[0].vel_y = backgrounds[1].vel_y = 700;
+    backgrounds[0].is_visible = true;
+    backgrounds[1].is_visible = false;
+    backgrounds[0].rect.w = backgrounds[1].rect.w = background_img_width;
+    backgrounds[0].rect.h = backgrounds[1].rect.h = background_img_height;
+
+    // Get player sprite dimensions
+    player.tex = player_tex;
+    SDL_QueryTexture(player.tex, NULL, NULL, &player.rect.w, &player.rect.h);
+    // Initialize player object
+    player.x = (WINDOW_WIDTH / 2) - (player.rect.w / 2);
+    player.y = 500;
+    player.vel_x = 300;
+    player.vel_y = 300;
+
+    // Get laser sprite dimensions
+    int laser_img_width, laser_img_height;
+    SDL_QueryTexture(laser_tex, NULL, NULL, &laser_img_width, &laser_img_height);
+    // Initialize lasers objects
+    for (int i = 0; i < sizeof(lasers) / sizeof(lasers[0]); i++) {
+        lasers[i].tex = laser_tex;
+        lasers[i].rect.w = laser_img_width;
+        lasers[i].rect.h = laser_img_height;
+        lasers[i].vel_y = 800;
+        lasers[i].is_visible = false;
+    }
+
+    // Define enemies sprites
+    for (int i = 0; i < sizeof(enemies) / sizeof(enemies[0]); i++) {
+        // Generate random index (j) to choose random sprite
+        int j = rand() % (sizeof(enemies_texs) / sizeof(enemies_texs[0]));
+        enemies[i].tex = enemies_texs[j];
+        // Get sprite dimensions
+        SDL_QueryTexture(enemies[i].tex, NULL, NULL, &enemies[i].rect.w, &enemies[i].rect.h);
+        // Initialize object
+        enemies[i].x = i * 100;
+        enemies[i].y = 150;
+        enemies[i].is_visible = false;
+    }
+
+    // Define asteroids sprites
+    for (int i = 0; i < sizeof(asteroids) / sizeof(asteroids[0]); i++) {
+        // Generate random index (j) to choose random sprite
+        int j = rand() % ((sizeof(asteroids_texs) / sizeof(asteroids_texs[0])));
+        asteroids[i].tex = asteroids_texs[j];
+        // Get sprite dimensions
+        SDL_QueryTexture(asteroids[i].tex, NULL, NULL, &asteroids[i].rect.w, &asteroids[i].rect.h);
+        // Initialize object
+        asteroids[i].x = i * 100;
+        asteroids[i].y = 0;
+        asteroids[i].is_visible = false;
+    }
+
+    // Get explosion sprite dimensions
+    int explosion_img_width, explosion_img_height;
+    SDL_QueryTexture(explosion_tex, NULL, NULL, &explosion_img_width, &explosion_img_height);
+    // Initialize explosion objects
+    for (int i = 0; i < sizeof(explosions) / sizeof(explosions[0]); i++) {
+        explosions[i].tex = explosion_tex;
+        explosions[i].rect.w = explosion_img_width / 1.5;
+        explosions[i].rect.h = explosion_img_height / 1.5;
+        explosions[i].is_visible = false;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Process input
 ///////////////////////////////////////////////////////////////////////////////
 void process_input(void) {
     SDL_Event event;
@@ -156,129 +273,6 @@ void process_input(void) {
     }
 }
 
-///////////////////////////////////////////////////////////////////////////////
-// Setup function that runs once at the beginning of our program
-///////////////////////////////////////////////////////////////////////////////
-void setup(void) {
-
-    // srand initialization - generate random numbers with rand()
-    srand(time(NULL));
-
-    // Create surfaces
-    SDL_Surface *background_sfc = IMG_Load("assets/background.png");
-    SDL_Surface *player_sfc = IMG_Load("assets/player.png");
-    SDL_Surface *laser_sfc = IMG_Load("assets/laser.png");
-    SDL_Surface *enemies_sfcs[14];
-    enemies_sfcs[0] = IMG_Load("assets/enemy-blue-01.png");
-    enemies_sfcs[1] = IMG_Load("assets/enemy-blue-02.png");
-    enemies_sfcs[2] = IMG_Load("assets/enemy-brown-01.png");
-    enemies_sfcs[3] = IMG_Load("assets/enemy-brown-02.png");
-    enemies_sfcs[4] = IMG_Load("assets/enemy-green-01.png");
-    enemies_sfcs[5] = IMG_Load("assets/enemy-green-02.png");
-    enemies_sfcs[6] = IMG_Load("assets/enemy-orange-01.png");
-    enemies_sfcs[7] = IMG_Load("assets/enemy-orange-02.png");
-    enemies_sfcs[8] = IMG_Load("assets/enemy-purple-01.png");
-    enemies_sfcs[9] = IMG_Load("assets/enemy-purple-02.png");
-    enemies_sfcs[10] = IMG_Load("assets/enemy-red-01.png");
-    enemies_sfcs[11] = IMG_Load("assets/enemy-red-02.png");
-    enemies_sfcs[12] = IMG_Load("assets/enemy-yellow-01.png");
-    enemies_sfcs[13] = IMG_Load("assets/enemy-yellow-02.png");
-    SDL_Surface *asteroids_sfcs[2];
-    asteroids_sfcs[0] = IMG_Load("assets/asteroid-01.png");
-    asteroids_sfcs[1] = IMG_Load("assets/asteroid-02.png");
-    SDL_Surface *explosion_sfc = IMG_Load("assets/explosion.png");
-
-    // Create textures
-    background_tex = SDL_CreateTextureFromSurface(renderer, background_sfc);
-    SDL_FreeSurface(background_sfc);
-    player_tex = SDL_CreateTextureFromSurface(renderer, player_sfc);
-    SDL_FreeSurface(player_sfc);
-    laser_tex = SDL_CreateTextureFromSurface(renderer, laser_sfc);
-    SDL_FreeSurface(laser_sfc);
-    for (int i = 0; i < sizeof(enemies_texs) / sizeof(enemies_texs[0]); i++) {
-        enemies_texs[i] = SDL_CreateTextureFromSurface(renderer, enemies_sfcs[i]);
-        SDL_FreeSurface(enemies_sfcs[i]);
-    }
-    for (int i = 0; i < sizeof(asteroids_texs) / sizeof(asteroids_texs[0]); i++) {
-        asteroids_texs[i] = SDL_CreateTextureFromSurface(renderer, asteroids_sfcs[i]);
-        SDL_FreeSurface(asteroids_sfcs[i]);
-    }
-    explosion_tex = SDL_CreateTextureFromSurface(renderer, explosion_sfc);
-
-    // Get background sprite dimensions
-    int background_img_width, background_img_height;
-    SDL_QueryTexture(background_tex, NULL, NULL, &background_img_width, &background_img_height);
-    // Initialize background object
-    backgrounds[0].x = backgrounds[1].x = 0;
-    backgrounds[0].y = WINDOW_HEIGHT - background_img_height;
-    backgrounds[1].y = - background_img_height;
-    backgrounds[0].vel_y = backgrounds[1].vel_y = 700;
-    backgrounds[0].is_visible = true;
-    backgrounds[1].is_visible = false;
-    backgrounds[0].rect.w = backgrounds[1].rect.w = background_img_width;
-    backgrounds[0].rect.h = backgrounds[1].rect.h = background_img_height;
-
-    // Get player sprite dimensions
-    player.tex = player_tex;
-    SDL_QueryTexture(player.tex, NULL, NULL, &player.rect.w, &player.rect.h);
-    // Initialize player object
-    player.x = (WINDOW_WIDTH / 2) - (player.rect.w / 2);
-    player.y = 500;
-    player.vel_x = 300;
-    player.vel_y = 300;
-
-    // Get laser sprite dimensions
-    int laser_img_width, laser_img_height;
-    SDL_QueryTexture(laser_tex, NULL, NULL, &laser_img_width, &laser_img_height);
-    // Initialize lasers objects
-    for (int i = 0; i < sizeof(lasers) / sizeof(lasers[0]); i++) {
-        lasers[i].tex = laser_tex;
-        lasers[i].rect.w = laser_img_width;
-        lasers[i].rect.h = laser_img_height;
-        lasers[i].vel_y = 800;
-        lasers[i].is_visible = false;
-    }
-
-    // Define enemies sprites
-    int j;
-    for (int i = 0; i < sizeof(enemies) / sizeof(enemies[0]); i++) {
-        // Generate random index (j) to choose random sprite
-        j = rand() % (sizeof(enemies_texs) / sizeof(enemies_texs[0]));
-        enemies[i].tex = enemies_texs[j];
-        // Get sprite dimensions
-        SDL_QueryTexture(enemies[i].tex, NULL, NULL, &enemies[i].rect.w, &enemies[i].rect.h);
-        // Initialize object
-        enemies[i].x = i * 100;
-        enemies[i].y = -(i * 150);
-        enemies[i].vel_y = 40 + (rand() % 200);
-        enemies[i].is_visible = false;
-    }
-
-    // Define asteroids sprites
-    for (int i = 0; i < sizeof(asteroids) / sizeof(asteroids[0]); i++) {
-        // Generate random index (j) to choose random sprite
-        j = rand() % ((sizeof(asteroids_texs) / sizeof(asteroids_texs[0])));
-        asteroids[i].tex = asteroids_texs[j];
-        // Get sprite dimensions
-        SDL_QueryTexture(asteroids[i].tex, NULL, NULL, &asteroids[i].rect.w, &asteroids[i].rect.h);
-        // Initialize object
-        asteroids[i].x = i * 100;
-        asteroids[i].y = -(i * 150);
-        asteroids[i].vel_y = 40 + (rand() % 200);
-        asteroids[i].is_visible = false;
-    }
-
-    // Get explosion sprite dimensions
-    int explosion_img_width, explosion_img_height;
-    SDL_QueryTexture(explosion_tex, NULL, NULL, &explosion_img_width, &explosion_img_height);
-    // Initialize explosion objects
-    for (int i = 0; i < sizeof(explosions) / sizeof(explosions[0]); i++) {
-        explosions[i].tex = explosion_tex;
-        explosions[i].rect.w = explosion_img_width / 1.5;
-        explosions[i].rect.h = explosion_img_height / 1.5;
-        explosions[i].is_visible = false;
-    }
-}
 
 ///////////////////////////////////////////////////////////////////////////////
 // Update function with a fixed time step
@@ -292,31 +286,31 @@ void update(void) {
 
     for (int i = 0; i < sizeof(backgrounds) / sizeof(backgrounds[0]); i++) {
         bool j = !i;
-        if (backgrounds[i].is_visible && backgrounds[i].y < WINDOW_HEIGHT){
+        if (backgrounds[i].is_visible && backgrounds[i].y < WINDOW_HEIGHT) {
             backgrounds[i].y += backgrounds[i].vel_y * delta_time;
             // Set SDL_Rect coordinates
             backgrounds[i].rect.x = backgrounds[i].x;
             backgrounds[i].rect.y = backgrounds[i].y;
-            if (backgrounds[i].y > 0){
+            if (backgrounds[i].y > 0) {
                 backgrounds[j].is_visible = true;
             }
         } else {
             backgrounds[i].is_visible = false;
-            backgrounds[i].y = - backgrounds[i].rect.h;
+            backgrounds[i].y = -backgrounds[i].rect.h;
         }
     }
 
     // Move player as a function of delta time
-    if (move_up && !move_down && player.y >= 0) {
+    if (move_up && !move_down && player.y > 0) {
         player.y -= player.vel_y * delta_time;
     }
-    if (move_down && !move_up && player.y <= WINDOW_HEIGHT - player.rect.h) {
+    if (move_down && !move_up && player.y < WINDOW_HEIGHT - player.rect.h) {
         player.y += player.vel_y * delta_time;
     }
-    if (move_left && !move_right && player.x >= 0) {
+    if (move_left && !move_right && player.x > 0) {
         player.x -= player.vel_x * delta_time;
     }
-    if (move_right && !move_left && player.x <= WINDOW_WIDTH - player.rect.w) {
+    if (move_right && !move_left && player.x < WINDOW_WIDTH - player.rect.w) {
         player.x += player.vel_x * delta_time;
     }
     // Set SDL_Rect coordinates
@@ -377,7 +371,7 @@ void update(void) {
     for (int i = 0; i < sizeof(explosions) / sizeof(explosions[0]); i++) {
         if (explosions[i].is_visible) {
             if (explosions[i].count < 10) {
-                // Duration increases as a function of delta time
+                // count increases as a function of delta time
                 explosions[i].count += 100 * delta_time;
             } else {
                 explosions[i].is_visible = false;
@@ -386,7 +380,7 @@ void update(void) {
         }
     }
 
-    // TODO: COLLISION DETECTION
+    // Collision detection
     // Laser X Enemy
     for (int i = 0; i < sizeof(lasers) / sizeof(lasers[0]); i++) {
         for (int j = 0; j < sizeof(enemies) / sizeof(enemies[0]); j++) {
@@ -512,7 +506,7 @@ void destroy_window(void) {
 int main(int argc, char *args[]) {
     game_is_running = initialize_window();
 
-    setup();
+    initial_setup();
 
     while (game_is_running) {
         process_input();
